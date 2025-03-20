c3b1b7f605ce5318ce5ed116ad6695882a10a297
uncpleo <leonardocqtc@gmail.com>
2025-03-20 12:30:01 -0500

    Cambios locales

--- Paraprobar/repository/database.py
+++ Paraprobar/repository/database.py
@@ -1,10 +1,18 @@
 from sqlmodel import create_engine, Session, select,SQLModel
 from ..models.excel_data import ExcelData,Reglas,Proyectos,Entregables
 from ..models.entregable_model import Entregable
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+# Leer las variables de entorno
+USER_ENV = os.getenv("USER_ENV")
+PASS_ENV = os.getenv("PASS_ENV")
+DATABASE_ENV = os.getenv("DATABASE_ENV")
 
 
 # Construir la URL de conexión a la base de datos
-DATABASE_URL = "mysql+mysqlconnector://root:@localhost/pro_bisa"
+DATABASE_URL = f"mysql+mysqlconnector://{USER_ENV}:{PASS_ENV}@localhost/{DATABASE_ENV}"
 engine = create_engine(DATABASE_URL)
 
 def init_db():
