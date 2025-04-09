49b2fed83a5f6b91ce1b7a5c0b5e74e54f485b2d
AndyFerEra <andy.erazofernandez@gmail.com>
2025-04-09 15:26:58 -0500

    Usando Sql Server

--- Paraprobar/repository/database.py
+++ Paraprobar/repository/database.py
@@ -1,16 +1,32 @@
-from sqlmodel import create_engine, Session, select,SQLModel
-from sqlalchemy.sql import text
+from sqlmodel import asc, create_engine, Session, select,SQLModel
 from ..models.excel_data import ExcelData,Reglas,Proyectos,Entregables,vistaentregablesproyectos
+from urllib.parse import quote_plus
 
 
-DATABASE_URL = "mysql+mysqlconnector://root:@localhost/pro_bisa"
+# Codificar la contraseña (por el carácter @)
+password = "12345"
+encoded_password = quote_plus(password)
+
+# Cadena de conexión
+DATABASE_URL = rf"mssql+pyodbc://sa:{encoded_password}@DESKTOP-L84HKA8\BISA/pro_bisa?driver=ODBC+Driver+17+for+SQL+Server"
 engine = create_engine(DATABASE_URL)
 
+# Inicializar la base de datos
 def init_db():
-    SQLModel.metadata.create_all(engine)
+    try:
+        SQLModel.metadata.create_all(engine)
+        print("Base de datos inicializada correctamente.")
+    except Exception as e:
+        print(f"Error al inicializar la base de datos: {e}")
 
+# Crear una sesión
 def get_session():
-    return Session(engine)
+    try:
+        return Session(engine)
+    except Exception as e:
+        print(f"Error al crear la sesión: {e}")
+        return None
+# Obtener todos los registros de la tabla "Entregable"
 
     
 #prueba
@@ -83,3 +99,4 @@ def select_entregables_con_proyectos():
         )
         results = session.exec(statement)
         return results.all()
+    
